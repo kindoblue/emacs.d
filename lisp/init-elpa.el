6b72047562f3510ce9c2b5fe6360745ee4bb4f04
Stefano Secondo <stefano.secondo@tomtom.com>
2014-10-08 13:41:22 +0200

    Reverted melpa rep

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -24,8 +24,10 @@
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 
 ;;; Also use Melpa for most packages
-(add-to-list 'package-archives '("melpa" . "http://melpa-stable.milkbox.net/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 
+(when (not package-archive-contents)
+  (package-refresh-contents))
 
 
 ;; If gpg cannot be found, signature checking will fail, so we

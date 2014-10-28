f20aaa2add5d603c3e847ece3297bce68912f785
Stefano S <kindoblue@gmail.com>
2014-10-28 19:45:55 +0100

    Revert "added skewer"
    This reverts commit 60ae90403cecf6919c10738d0c24fab94f53cecb.

--- init.el
+++ init.el
@@ -99,9 +99,6 @@
 (require 'init-rails)
 (require 'init-sql)
 
-;; (require 'init-ggtags)
-(require 'init-skewer)
-
 (require 'init-paredit)
 (require 'init-lisp)
 (require 'init-slime)
--- lisp/init-skewer.el
+++
@@ -1,8 +0,0 @@
-(require-package 'skewer-mode)
-
-(after-load 'skewer-mode
-  (add-hook 'js2-mode-hook 'skewer-mode)
-  (add-hook 'css-mode-hook 'skewer-css-mode)
-  (add-hook 'html-mode-hook 'skewer-html-mode))
-
-(provide 'init-skewer)

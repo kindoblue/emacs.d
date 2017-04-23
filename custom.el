a164925584bdbbc4a84270f02cd57b4de0fbf458
Stefano S <kindoblue@gmail.com>
2017-04-23 18:19:43 +0200

    custom conf file added

---
+++ custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (sanityinc-solarized-dark)))
+ '(package-selected-packages
+   (quote
+    (scala-mode2 yari yaml-mode yagist whole-line-or-region whitespace-cleanup-mode wgrep virtualenvwrapper vc-darcs unfill undo-tree tidy textile-mode tagedit switch-window sql-indent smex smarty-mode slime-company skewer-less session scss-mode scratch sass-mode ruby-hash-syntax rspec-mode robe rinari redshank rainbow-mode rainbow-delimiters projectile-rails project-local-variables pip-requirements php-mode paredit-everywhere page-break-lines org-pomodoro org-mac-iCal org-fstree org-cliplink nlinum multiple-cursors move-dup mmm-mode markdown-mode magit-gh-pulls lively less-css-mode json-mode js-comint jedi ivy-historian ipretty intero indent-guide immortal-scratch ibuffer-vc hl-sexp hippie-expand-slime hindent highlight-symbol highlight-quoted highlight-escape-sequences hayoo guide-key grab-mac-link goto-gem gitignore-mode github-issues github-clone gitconfig-mode git-timemachine git-messenger git-blamed fullframe flycheck-package flycheck-elm flycheck-clojure fill-column-indicator expand-region exec-path-from-shell erlang ensime elpy elm-mode elisp-slime-nav elein disable-mouse dired-sort dired+ diminish diff-hl default-text-scale darcsum csv-nav csv-mode css-eldoc crontab-mode counsel company-quickhelp command-log-mode color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized coffee-mode cmd-to-echo cljsbuild-mode cl-lib-highlight cask-mode bundler bug-reference-github browse-kill-ring browse-at-remote avy auto-compile anzu aggressive-indent)))
+ '(session-use-package t nil (session)))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(font-lock-comment-face ((t (:foreground "chocolate1" :slant italic)))))
+
+;;; cystom.el ends here
